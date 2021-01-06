1472a8e103462915a84d5c7fe1d7bd22a1c9273b
typhonrt <support@typhonrt.org>
2021-01-06 09:15:04 -0800

    removed FlagHandler and FileUtil

--- src/index.js
+++ src/index.js
@@ -1,10 +1,4 @@
 // Errors
 const NonFatalError  = require('./error/NonFatalError');
 
-// Utils
-const FileUtil       = require('./util/FileUtil');
-const FlagHandler    = require('./util/FlagHandler');
-
 exports.NonFatalError = NonFatalError;
-exports.FileUtil = FileUtil;
-exports.FlagHandler = FlagHandler;
