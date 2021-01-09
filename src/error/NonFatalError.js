64f1c466da01bae096bd0349679972c257f8c9e6
typhonrt <support@typhonrt.org>
2021-01-09 08:17:14 -0800

    added parsing for log level qualifier.

--- src/error/NonFatalError.js
+++ src/error/NonFatalError.js
@@ -20,46 +20,82 @@ class NonFatalError extends Error
 
       // Sanitize incoming data.
       let logEvent = 'log:error';
+      let logQualifer = '';
+
       errorCode = Number.isInteger(errorCode) ? errorCode : 1;
 
-      // Set logEvent and errorCode based on logLevel.
-      if (typeof logLevel === 'string')
+      this.$$logEvent = logEvent;
+      this.$$errorCode = errorCode;
+
+      // Early out if logLevel is not a string
+      if (typeof logLevel !== 'string')
       {
-         switch (logLevel)
+         return;
+      }
+
+      const results = logLevel.split(':');
+
+      // Parse log level qualifier
+      if (results.length > 1 && typeof results[1] === 'string')
+      {
+         switch (results[1])
+         {
+            case 'compact':
+               logQualifer = ':compact';
+               break;
+
+            case 'nocolor':
+               logQualifer = ':nocolor';
+               break;
+
+            case 'raw':
+               logQualifer = ':raw';
+               break;
+
+            case 'time':
+               logQualifer = ':time';
+               break;
+         }
+      }
+
+      // Set logEvent and errorCode based on logLevel.
+      if (typeof results[0] === 'string')
+      {
+         switch (results[0])
          {
             case 'fatal':
-               logEvent = 'log:fatal';
+               logEvent = `log:fatal${logQualifer}`;
                errorCode = 2;
                break;
 
             case 'error':
-               logEvent = 'log:error';
+               logEvent = `log:error${logQualifer}`;
                errorCode = 1;
                break;
 
             case 'warn':
-               logEvent = 'log:warn';
+               logEvent = `log:warn${logQualifer}`;
                errorCode = 0;
                break;
 
             case 'info':
-               logEvent = 'log:info';
+               logEvent = `log:info${logQualifer}`;
                errorCode = 0;
                break;
 
             case 'verbose':
-               logEvent = 'log:verbose';
+               logEvent = `log:verbose${logQualifer}`;
                errorCode = 0;
                break;
 
             case 'debug':
-               logEvent = 'log:debug';
+               logEvent = `log:debug${logQualifer}`;
                errorCode = 0;
                break;
 
             case 'trace':
-               logEvent = 'log:trace';
-               errorCode = 3;
+               logEvent = `log:trace${logQualifer}`;
+               errorCode = 0;
                break;
          }
       }
